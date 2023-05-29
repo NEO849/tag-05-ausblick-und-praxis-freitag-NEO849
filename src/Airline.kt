eb76ca0ce5dc375a956fad9462c59aae6015c626
neo <f.michi84.989@gmail.com>
2023-05-29 20:22:07 +0200

    -var - val überdacht

--- src/Airline.kt
+++ src/Airline.kt
@@ -1,18 +1,22 @@
 fun main() {
 
-    val sitzplaetzeGesamt = 130
-    var kostenProTicket = 1000
-    var familienProFlug = sitzplaetzeGesamt / 4
-    var freieSitzplaetze = sitzplaetzeGesamt - (25 * 4)
-    val kmMadridNachMailand = 1034.93
-    var preisProKm = 16
-    var einnahmen = sitzplaetzeGesamt * kostenProTicket
-    var flugkosten = kmMadridNachMailand * preisProKm
-    var gewinn = einnahmen - flugkosten
-
     val reset = "\u001B[0m"  //die farbe würde so ausschauen: val weis = "\u001B[37m"
     val rot = "\u001B[31m"   //systemtechnisch ist weis eingestellt
     val gruen = "\u001B[32m" //deshalb reset denke ich
+
+    val kmMadridNachMailand = 1034.93
+    val sitzplaetzeGesamt = 130
+
+    var kostenProTicket = 1000
+    var anzahlDerFamilien = 25
+    var koepfeProFamilie = 4
+    var preisProKm = 16
+
+    var familienProFlug = sitzplaetzeGesamt / koepfeProFamilie
+    var freieSitzplaetze = sitzplaetzeGesamt - (anzahlDerFamilien * koepfeProFamilie)
+    var einnahmen = sitzplaetzeGesamt * kostenProTicket
+    var flugkosten = kmMadridNachMailand * preisProKm
+    var gewinn = einnahmen - flugkosten
 
     val gewinnAnzeige = if (gewinn >= 0) "$gruen$gewinn$reset" else "$rot$gewinn$reset"
 
